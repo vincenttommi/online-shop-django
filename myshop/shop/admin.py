0649156280ad8336e414eedec1ecf05e506c3868
vincenttommi <vincenttommi@gmail.com>
2024-01-05 04:19:27 +0300

    updating models

--- myshop/shop/admin.py
+++ myshop/shop/admin.py
@@ -1,24 +1,24 @@
-#Registering catalog models on the adminstration site
+#Registering catalog  models on the administration site
 from django.contrib import admin
 from .models import Category,Product
 
 
 
-
 @admin.register(Category)
 class CategoryAdmin(admin.ModelAdmin):
-    list_display = ['name', 'slug']
+    list_display = ['name','slug']
     prepopulated_fields = {'slug':('name',)}
-    #used to specify fields where the value  is automatically set using value of other fields
     
     
-    
-
 
 @admin.register(Product)
 class ProductAdmin(admin.ModelAdmin):
     list_display = ['name','slug','price','available','created','updated']
-    list_filter = ['available', 'created', 'updated']
-    list_editable = ['price', 'available']
+    list_filter = ['available','created','updated']
+    list_editable = ['price','available']
     prepopulated_fields = {'slug':('name',)}
-    
+    #this field specifies where the  value is automatically set using value of other fields
+       
+       
+       
+#adding models to  categories so I can easily manage categories and Products 
--- myshop/shop/models.py
+++ myshop/shop/models.py
@@ -1,60 +1,39 @@
 from django.db import models
 
-
-#Category model
 class Category(models.Model):
-    
     name = models.CharField(max_length=200)
-    slug =  models.SlugField(max_length=200, unique=True)
-    
-    
+    slug = models.SlugField(max_length=200, unique=True)
+
     class Meta:
-        ordering  = ['name']
+        ordering = ['name']
         indexes = [
             models.Index(fields=['name']),
         ]
-        
         verbose_name = 'category'
-        verbose_name_plural  = 'categories'
-        
-        
-        def __str__(self):
-            return  self.name
-        
-        
-        
-        
-#Product model 
+        verbose_name_plural = 'categories'
+
+    def __str__(self):
+        return self.name
+
+
 class Product(models.Model):
-    category =models.ForeignKey(Category, related_name='products', on_delete=models.CASCADE)
-    #A Foreignkey to the Category model.This is a one to many relationship
-    #a product belongs to  one category and category contains multiple products
-    name  = models.CharField(max_length=200)
-    #name of the product 
-    slug = models.SlugField(max_length=200)
-    #Slug for  product to build beautiful URLs
+    category = models.ForeignKey(Category, related_name='products', on_delete=models.CASCADE)
+    name = models.CharField(max_length=200)
+    slug = models.SlugField(max_length=200, unique=True)
     image = models.ImageField(upload_to='products/%Y/%m/%d', blank=True)
-    #field for capturing optional product image
-    
     description = models.TextField(blank=True)
-    price  = models.DecimalField(max_digits=10, decimal_places=2)
-    #The field uses  python decimal.Decimal type to  store a fixed-precision decimal number
-    available =  models.BooleanField(default=True)
-    #Boolean value used to indicate wether product is  available or not
-    # used to enable/disable the product in the catalog
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    available = models.BooleanField(default=True)
     created = models.DateTimeField(auto_now_add=True)
-    #stores when the product was created
-    updated  =  models.DateTimeField(auto_now=True)
-    #Store when the object was last updated 
-    
-    
+    updated = models.DateTimeField(auto_now=True)
+
     class Meta:
-        ordering  = ['name']
-        indexes  = [
+        ordering = ['name']
+        indexes = [
             models.Index(fields=['id', 'slug']),
             models.Index(fields=['name']),
-            models.Index(fields=['-created'])
+            models.Index(fields=['-created']),
         ]
-        
+
     def __str__(self):
-        return self.name    
+        return self.name
