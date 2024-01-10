96f6be1211017a4777779d11a4db29eef4fa3eea
vincenttommi <vincenttommi@gmail.com>
2024-01-10 08:27:30 +0300

    registering catalog models on administration site

--- myshop/shop/admin.py
+++ myshop/shop/admin.py
@@ -4,17 +4,15 @@ from .models import Category,Product
 
 @admin.register(Category)
 class CategoryAdmin(admin.ModelAdmin):
-    list_display = ['name','slug']
+    list_display = ['name', 'slug']
     prepopulated_fields = {'slug':('name',)}
-
-
-
-
+    
+    
+    
+    
 @admin.register(Product)
-class ProductAdmin(admin.ModelAdmin):
-    list_display = ['name','slug','price','available','created','updated']
+class  ProductAdmin(admin.ModelAdmin):
+    list_display  = ['name','slug','price','available','created','updated']
     list_filter = ['available','created','updated']
     list_editable = ['price','available']
     prepopulated_fields = {'slug':('name',)}
-    #specify  fields  where  the value is automatically set using the value of
-    #other fields
--- myshop/shop/models.py
+++ myshop/shop/models.py
@@ -25,6 +25,17 @@ class Product(models.Model):
     price = models.DecimalField(max_digits=10, decimal_places=2)
     available = models.BooleanField(default=True)
     created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
 
+    
+    class Meta:
+        ordering = ['name']
+        indexes = [
+            models.Index(fields=['id','slug']),
+            models.Index(fields=['name']),
+            models.Index(fields=['-created']),
+        ]
+        
     def __str__(self):
         return self.name
+    
