8111696052c888a74bdb71f7f670e7b6da76ba07
vincenttommi <vincenttommi@gmail.com>
2024-01-10 09:22:29 +0300

    adding views

--- myshop/shop/views.py
+++ myshop/shop/views.py
@@ -1,3 +1,34 @@
-from django.shortcuts import render
+from django.shortcuts import render,get_list_or_404
+from .models import Category,Product
 
-# Create your views here.
+
+
+
+#View for retrieving available products
+def Product_list(request,category_slug=None):
+    Category = None
+    categories  = Category.objects.all()
+    products = Product.objects.filter(available=True)
+    
+    if category_slug:
+        category = get_object_or_404(Category, slug=category_slug)
+        products = products.filter(category=category)
+        
+        return render(request, 'shop/product/list.html',{'categories':categories,'products':products})
+    #Filtering the QuerSet with available=True to retrieve only available products
+    
+    
+
+
+#view for retrieving a single product
+
+
+def product_detail(request, id, slug):
+    product = get_object_or_404(Product, id=id,slug=slug,available=True)
+    
+    return render(request,'shop/product/detail.html',{'product':product})
+
+ 
+    
+    
+    
