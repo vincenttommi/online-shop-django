6877e301d38ce2fdc6c1717fa1c746efab1b8b75
vincenttommi <vincenttommi@gmail.com>
2024-01-22 17:39:35 +0300

    adding orders app

--- myshop/myshop/settings.py
+++ myshop/myshop/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'shop.apps.ShopConfig',
     'cart.apps.CartConfig',
+    'orders.apps.OrdersConfig',
 ]
 
 MIDDLEWARE = [
@@ -64,10 +65,16 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'cart.context_processors.cart',
+                
+                #cart context processor will be executed everytime a template is rendered using 
+                #DjangoRequestContext
             ],
         },
     },
 ]
+
+
 
 WSGI_APPLICATION = 'myshop.wsgi.application'
 
