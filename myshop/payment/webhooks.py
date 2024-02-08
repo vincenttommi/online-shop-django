a105b67380931f81f068da351ec520f311c1dda1
vincenttommi <vincenttommi@gmail.com>
2024-02-08 08:11:10 +0300

    webhooks for receiving payments

---
+++ myshop/payment/webhooks.py
@@ -0,0 +1,28 @@
+import  stripe
+from django.conf import settings
+from django.http import HttpResponse
+from django.views.decorators.csrf import csrf_exempt
+from orders.models import Order
+
+
+
+
+
+@csrf_exempt
+def stripe_webhook(request):
+    payload = request.body
+    
+    sig_header = request.META['HTTP_STRIPE_SIGNATURE']
+    event = None
+    
+    
+    try:
+        event = stripe.Webhook.construct_event(
+            payload,
+            sig_header,
+            settings.STRIPE_WEBHOOK_SECRET)
+        
+    except  ValueError as e:
+        #Invalid payload
+        return HttpResponse(status=400)
+    
